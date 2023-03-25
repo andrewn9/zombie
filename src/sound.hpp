a173f02f410e1d270bc0fb9537d9fc63a505a512
Luke Lawlor <lklawlor1@gmail.com>
2023-03-25 14:03:28 -0400

    added sound system

---
+++ src/sound.hpp
@@ -0,0 +1,32 @@
+/*
+ * sound.hpp
+ *
+ * sick sound system for a sick game
+ */
+
+#include <string>
+
+#include <SDL2/SDL_mixer.h>
+
+#pragma once
+
+namespace snd
+{
+	typedef Mix_Chunk Sfx;
+
+	// Sound effects
+	extern Sfx *square;
+	extern Sfx *snare;
+
+	// Sound system
+	struct SoundSystem{
+	private:
+		void loadAllSfx();
+		void freeAllSfx();
+		Sfx *loadSfx(std::string path);
+	public:
+		SoundSystem();
+		~SoundSystem();
+		void playSfx(Sfx *sfx);
+	};
+}
